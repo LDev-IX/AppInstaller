a41b65cdf7f6439a1eca553aa91ae8964b68b697
LDev_IX <48771081+LDev-IX@users.noreply.github.com>
2023-01-28 21:21:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace AppInstaller;
+
+static class Program{
+    static void Main(){
+        string var_current_dir = Environment.CurrentDirectory;
+        string var_app_name = "Unknown";
+        int var_current_line = 0;
+
+        if(!File.Exists(var_current_dir + @"\.lixi")){
+            return;
+        }
+
+        foreach(string fLine in File.ReadAllLines(var_current_dir + @"\.lixi")){
+            switch(var_current_line){
+                case 0:
+                    var_app_name = fLine;
+                    Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name);
+                    Console.WriteLine("Installing " + var_app_name);
+                break;
+
+                case 1:
+                    if(fLine == "Shortcut"){
+                        Console.WriteLine("Creating desktop shortcut");
+                        File.Copy(var_current_dir + @"\assets\" + var_app_name + @".exe", @"C:\ProgramData\" + var_app_name + @"\" +  var_app_name + @".exe", true);
+                        File.CreateSymbolicLink(@"C:\Users\" + Environment.UserName + @"\Desktop\" + var_app_name, @"C:\ProgramData\" + var_app_name + @"\" + var_app_name + @".exe");
+                    }
+                break;
+
+                default:
+                    if(fLine.StartsWith("mkdir ")){
+                        Console.WriteLine("Creating directory " + @"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace("mkdir ", ""));
+                        Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace("mkdir ", ""));
+                    }else{
+                        Console.WriteLine("Copying " + var_current_dir + @"\" + fLine + " to " + @"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace(@"assets\", ""));
+                        File.Copy(var_current_dir + @"\" + fLine, @"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace(@"assets\", ""), true);
+                    }
+                break;
+            }
+            var_current_line++;
+        }
+        Console.WriteLine("Done!");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
