2ff2b28abeeef24e0ba52a22865bc7cb5fc0ed04
LDev_IX <48771081+LDev-IX@users.noreply.github.com>
2023-02-04 18:09:54 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,51 +8,8 @@ static class Program{
     static string var_app_name = "Null";
 
     static void Main(){
-        string var_current_dir = Environment.CurrentDirectory;
-        string var_app_name = "Unknown";
-        int var_current_line = 0;
-
-        if(!File.Exists(var_current_dir + @"\.lixi")){
-            Console.WriteLine(".lixi file not found!");
-            return;
-        }
-
-        foreach(string fLine in File.ReadAllLines(var_current_dir + @"\.lixi")){
-            switch(var_current_line){
-                case 0:
-                    var_app_name = fLine;
-                    Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name);
-                    Console.WriteLine("Installing " + var_app_name);
-                break;
-
-                case 1:
-                    if(fLine == "Shortcut"){
-                        Console.WriteLine("Creating desktop shortcut");
-                        File.Copy(var_current_dir + @"\assets\" + var_app_name + @".exe", @"C:\ProgramData\" + var_app_name + @"\" +  var_app_name + @".exe", true);
-                        File.CreateSymbolicLink(@"C:\Users\" + Environment.UserName + @"\Desktop\" + var_app_name, @"C:\ProgramData\" + var_app_name + @"\" + var_app_name + @".exe");
-                    }
-                break;
-
-                default:
-                    if(fLine.StartsWith("mkdir ")){
-                        Console.WriteLine("Creating directory " + @"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace("mkdir ", ""));
-                        Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace("mkdir ", ""));
-                    }else{
-                        Console.WriteLine("Copying " + var_current_dir + @"\" + fLine + " to " + @"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace(@"assets\", ""));
-                        File.Copy(var_current_dir + @"\" + fLine, @"C:\ProgramData\" + var_app_name + @"\" + fLine.Replace(@"assets\", ""), true);
-                    }
-                break;
-            }
-            var_current_line++;
-        }
-        Console.WriteLine("Done!");
-        Console.WriteLine("Press any key to exit");
-        Console.ReadKey();
-    }
-
-    static void Rework(){
         string met_method = "Null";
-        string[] met_args = new string[4];
+        string[] met_args = {"Null", "Null", "Null", "Null"};
 
         if(!File.Exists(var_working_dir + @"\.lixi")){
             Console.WriteLine(".lixi file not found!");
@@ -60,8 +17,9 @@ static class Program{
         }
 
         foreach(string f_line in File.ReadAllLines(var_working_dir + @"\.lixi")){
-            met_method = f_line.Split('(', 1)[0];
-            met_args = (f_line.Replace(");", "").Replace(" ", "")).Split(',', 4);
+            string f_temp = f_line.Replace(" ", "").Replace("(", ":").Replace(");", "");
+            met_method = f_temp.Split(':', 2)[0];
+            met_args = f_temp.Split(':', 2)[1].Split(',', 4);
             if(!ExecuteMethod(met_method, met_args)){
                 return;
             }
@@ -76,6 +34,7 @@ static class Program{
             
             case "set_app_name":
                 var_app_name = arg_Arguments[0];
+                Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name);
                 break;
 
             case "create_shortcut":
@@ -84,6 +43,10 @@ static class Program{
 
             case "move":
                 File.Copy(var_working_dir + @"\assets\" + arg_Arguments[0], @"C:\ProgramData\" + var_app_name + @"\" + arg_Arguments[1], true);
+                break;
+
+            case "mkdir":
+                Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name + @"\" + arg_Arguments[0]);
                 break;
         }
         return true;
