5931c278d60130604220f650cf3a6137af747d5b
LDev_IX <48771081+LDev-IX@users.noreply.github.com>
2023-02-04 20:30:25 +0100

    Another major rework for v2.0.0

--- Program.cs
+++ Program.cs
@@ -4,51 +4,135 @@ using System.IO;
 namespace AppInstaller;
 
 static class Program{
-    static string var_working_dir = Environment.CurrentDirectory;
-    static string var_app_name = "Null";
+    static App constructed_app = new App();
+    static string working_directory = Environment.CurrentDirectory;
 
     static void Main(){
-        string met_method = "Null";
-        string[] met_args = {"Null", "Null", "Null", "Null"};
-
-        if(!File.Exists(var_working_dir + @"\.lixi")){
-            Console.WriteLine(".lixi file not found!");
+        if(!File.Exists(working_directory + @"\.lixi")){
+            Console.WriteLine("Error: .lixi file not found!");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
             return;
         }
 
-        foreach(string f_line in File.ReadAllLines(var_working_dir + @"\.lixi")){
+        int current_index = 0;
+        for(current_index = 0; current_index < 256; current_index++){
+            constructed_app.commands[current_index].name = "unset";
+        }
+
+        current_index = 0;
+        foreach(string f_line in File.ReadAllLines(working_directory + @"\.lixi")){
             string f_temp = f_line.Replace(" ", "").Replace("(", ":").Replace(");", "");
-            met_method = f_temp.Split(':', 2)[0];
-            met_args = f_temp.Split(':', 2)[1].Split(',', 4);
-            if(!ExecuteMethod(met_method, met_args)){
-                return;
+            Command constructed_cmd = new Command();
+            constructed_cmd.name = f_temp.Split(':', 2)[0];
+            constructed_cmd.arguments = f_temp.Split(':', 2)[1].Split(',', 2);
+            constructed_app.commands[current_index] = constructed_cmd;
+            current_index++;
+        }
+
+        current_index = 0;
+        foreach(Command f_command in constructed_app.commands){
+            if(f_command.name == "unset"){
+                break;
+            }
+            if(LoadAppProperties(f_command)){
+                constructed_app.commands[current_index].name = "propcmd";
+            }
+            current_index++;
+        }
+
+        Console.WriteLine("App info:");
+        Console.WriteLine("Name: " + constructed_app.name);
+        Console.WriteLine("Description: " + constructed_app.description);
+        Console.WriteLine("Made by: " + constructed_app.author);
+        Console.WriteLine("Website: " + constructed_app.website);
+        Console.WriteLine("");
+        Console.WriteLine("Do you want to install this app? [Y/n]");
+
+        if(Console.ReadLine().ToLower() != "y"){
+            return;
+        }
+
+        Console.Clear();
+        Console.WriteLine("Installing " + constructed_app.name);
+
+        foreach(Command f_command in constructed_app.commands){
+            if(f_command.name == "unset"){
+                break;
+            }
+            if(f_command.name != "propcmd"){
+                if(!ExecuteCommand(f_command)){
+                    Console.WriteLine("Error: Incorrect .lixi file syntax!");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey();
+                    return;
+                }
             }
         }
+
+        Console.WriteLine("Done!");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
     }
 
-    static bool ExecuteMethod(string arg_Name, string[] arg_Arguments){
-        switch(arg_Name){
+    static bool LoadAppProperties(Command command){
+        switch(command.name){
             default:
-                Console.WriteLine(".lixi method \"" + arg_Name + "\" not found!");
                 return false;
-            
-            case "set_app_name":
-                var_app_name = arg_Arguments[0];
-                Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name);
+
+            case "set_name":
+                constructed_app.name = command.arguments[0];
                 break;
 
-            case "create_shortcut":
-                File.CreateSymbolicLink(@"C:\Users\" + Environment.UserName + @"\Desktop\" + var_app_name, @"C:\ProgramData\" + var_app_name + @"\" + arg_Arguments[0]);
+            case "set_description":
+                constructed_app.description = command.arguments[0];
                 break;
 
-            case "move":
-                File.Copy(var_working_dir + @"\assets\" + arg_Arguments[0], @"C:\ProgramData\" + var_app_name + @"\" + arg_Arguments[1], true);
+            case "set_author":
+                constructed_app.author = command.arguments[0];
                 break;
 
-            case "mkdir":
-                Directory.CreateDirectory(@"C:\ProgramData\" + var_app_name + @"\" + arg_Arguments[0]);
+            case "set_website":
+                constructed_app.website = command.arguments[0];
                 break;
         }
         return true;
     }
+
+    static bool ExecuteCommand(Command command){
+        Console.WriteLine(command.name);
+        switch(command.name){
+            default:
+                return false;
+            
+            case "shortcut":
+                Console.WriteLine("Creating a desktop shortcut");
+                File.CreateSymbolicLink(@"C:\Users\" + Environment.UserName + @"\Desktop\" + constructed_app.name, @"C:\ProgramData\" + constructed_app.name + @"\" + command.arguments[0]);
+                break;
+
+            case "move":
+                Console.WriteLine("Moving files");
+                File.Copy(working_directory + @"\assets\" + command.arguments[0], @"C:\ProgramData\" + constructed_app.name + @"\" + command.arguments[1], true);
+                break;
+
+            case "mkdir":
+                Console.WriteLine("Creating directories");
+                Directory.CreateDirectory(@"C:\ProgramData\" + constructed_app.name + @"\" + command.arguments[0]);
+                break;
+        }
+        return true;
+    }
+}
+
+public class App{
+    public string name = "Null";
+    public string description = "Null";
+    public string author = "Null";
+    public string website = "Null";
+    public Command[] commands = new Command[256];
+}
+
+public class Command{
+    public string name = "Null";
+    public string[] arguments = new string[2];
 }
