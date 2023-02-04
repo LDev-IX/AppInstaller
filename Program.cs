9de6deb7d9beab47d60fd8bd7ef60f857fa9cb81
LDev_IX <48771081+LDev-IX@users.noreply.github.com>
2023-02-04 14:36:51 +0100

    More reworks

--- Program.cs
+++ Program.cs
@@ -51,13 +51,41 @@ static class Program{
     }
 
     static void Rework(){
+        string met_method = "Null";
+        string[] met_args = new string[4];
+
         if(!File.Exists(var_working_dir + @"\.lixi")){
             Console.WriteLine(".lixi file not found!");
             return;
         }
 
         foreach(string f_line in File.ReadAllLines(var_working_dir + @"\.lixi")){
-            
+            met_method = f_line.Split('(', 1)[0];
+            met_args = (f_line.Replace(");", "").Replace(" ", "")).Split(',', 4);
+            if(!ExecuteMethod(met_method, met_args)){
+                return;
+            }
         }
+    }
+
+    static bool ExecuteMethod(string arg_Name, string[] arg_Arguments){
+        switch(arg_Name){
+            default:
+                Console.WriteLine(".lixi method \"" + arg_Name + "\" not found!");
+                return false;
+            
+            case "set_app_name":
+                var_app_name = arg_Arguments[0];
+                break;
+
+            case "create_shortcut":
+                File.CreateSymbolicLink(@"C:\Users\" + Environment.UserName + @"\Desktop\" + var_app_name, @"C:\ProgramData\" + var_app_name + @"\" + arg_Arguments[0]);
+                break;
+
+            case "move":
+                File.Copy(var_working_dir + @"\assets\" + arg_Arguments[0], @"C:\ProgramData\" + var_app_name + @"\" + arg_Arguments[1], true);
+                break;
+        }
+        return true;
     }
 }
